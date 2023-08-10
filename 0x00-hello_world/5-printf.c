da6911d28cdf968737810b75e4fbff4e85307e33
ayegbakenneth <ayegbakenneth>
2023-08-10 11:24:54 -0700

    5-printf.c C program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that print exactly with proper grammer
+ * Return: 0 (true)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n")
+	return (0);
+}
