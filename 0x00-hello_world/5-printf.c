c892a14841db1cab1b864887227f3bbbeb635b13
ayegbakenneth <ayegbakenneth>
2023-08-10 12:25:32 -0700

    done successfully

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - This is the starting point
+ *
  * Return: 0 (true)
  */
+
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n")
+printf("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
 }
