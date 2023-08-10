7db13f0fbe5f3216fe8f08311cbc5a3552698f9e
ayegbakenneth <ayegbakenneth>
2023-08-10 14:23:40 -0700

    C program file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", sizedof(char));
+	printf("size of int:%lu byte(s)", sizeof(int));
+	printf("size of a long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float);
+	return (0);
+}
