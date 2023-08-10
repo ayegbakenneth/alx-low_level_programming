a0a538283dcdcde81628636877eb3b8af9ba3bcc
ayegbakenneth <ayegbakenneth>
2023-08-10 06:10:46 -0700

    a C program commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the starting point of this program
+ * Return: 0 (true)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
