d8a46a45fa09b49db7e2cabbb815d3355d0fa2ca
ayegbakenneth <ayegbakenneth>
2023-08-12 05:00:34 -0700

    6-size.c commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - main function
+ * main - Entry point
  *
- * Return: 0 for success
+ * Return: Always 0 when successfull
  */
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)", sizedof(char));
+	printf("Size of a char: %lu byte(s)", sizeof(char));
 	printf("size of int:%lu byte(s)", sizeof(int));
-	printf("size of a long int: %lu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float);
+	printf("size of a long int:%lu byte(s)", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
 	return (0);
 }
