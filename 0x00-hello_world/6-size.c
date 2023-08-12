10c095c62fc99115894406d9a0a83a8e7e9323d9
ayegbakenneth <ayegbakenneth>
2023-08-12 05:42:20 -0700

    6-size.c final commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 	long int o;
 	long long int p;
-	float f;
+	float q;
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(m));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(n));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(o));
