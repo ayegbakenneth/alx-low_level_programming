3ebb7a938927b50cf0262cf17051cd4079e94d51
ayegbakenneth <ayegbakenneth>
2023-08-12 05:37:19 -0700

    6-size.c new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,19 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 when successfull
+ * Return: Always 0 when success
  */
-
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)", sizeof(char));
-	printf("size of int:%lu byte(s)", sizeof(int));
-	printf("size of a long int:%lu byte(s)", sizeof(long int));
-	printf("size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
-	return (0);
+	char m;
+	int n;
+	long int o;
+	long long int p;
+	float f;
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(m));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(n));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(o));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(p));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(q));
+return (0);
 }
