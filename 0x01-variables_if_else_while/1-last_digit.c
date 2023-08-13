8c6b74f85fed6268efff9fcd537cc399fe7a1346
ayegbakenneth <ayegbakenneth>
2023-08-12 23:22:41 -0700

    2-print_alphabet.c C program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- * main - Entry
- * Return: Always 0
- */
+ * main - Entry point
+ *
+ *Description: last digit
+ *
+ *Return: Always 0 (success)
+*/
+
 int main(void)
 {
 	int n;
-	int a;
+	int last_digit;
+
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	a = n % 10;
-	if (n > 5)
-	{
-		printf("last digit of %d is %d and is and is greater than 5\n"
+	last_digit = n % 10;
+
+	if (last_digit > 5)
+		printf("last digit of %d is %d and is and is greater than 5\n", n, last_digit);
+	else if (last_digit == 0)
+		printf("last digit of %d is %d and is 0\n", n, last_digit);
+	else
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the program starting point
+ *Description: printing alphabet in lowercase
+ *Return: Always 0 when successfull
+*/
+int main(void)
+{
+	char ken
+
+	for (ken = 'a'; ken <= 'z'; ken++)
+	{
+		putchar(ken);
+	}
+	putchar('\n');
+	return (0);
+}
