8498fd77e1a2974dffc81b3084fa05f6aadb2d26
ayegbakenneth <ayegbakenneth>
2023-08-13 08:15:21 -0700

    succesfull

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char number;
+	for (number = '0'; number <= 'f'; number++)
+		putchar(number);
+	for (number = 'a'; number <= 'f'; number++)
+		putchar(number);
+	putchar('\n');
+	return (0);
+}
