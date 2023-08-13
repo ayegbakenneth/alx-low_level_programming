a5ad8ee8cf9ad192545b1f54abf58e038384251a
ayegbakenneth <ayegbakenneth>
2023-08-13 00:27:37 -0700

    3-print_alphabets.c C program

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabelt;
+
+	for (alphabelt = 'a'; alphabelt <= 'z'; alphabelt++)
+		putchar(alphabelt);
+	for (alphabelt = 'A'; alphabelt <= 'Z'; alphabelt++)
+		puchar(alphabelt);
+	putchar('\n')
+	return (0);
+}
+
