91fa4d1b4ffb7462e5b4e964bf85147d06ff99b5
ayegbakenneth <ayegbakenneth>
2023-08-13 01:22:11 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-last_digit.c -o 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Entry point
- *
- *Description: last digit
+ * main - Program entry point
  *
  *Return: Always 0 (success)
 */
 int main(void)
 {
-	int n, last_dij
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_dij = n % 10;
+	printf("last digit of %d is %d ", n, n % 10);
 
-	if (last_dij > 5)
-		printf("last dij of %d is %d and is and is greater than 5\n", n, last_dij);
-	else if (last_dij == 0)
-		printf("last dij of %d is %d and is 0\n", n, last_dij);
+	if (n % 10 > 5)
+		printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf("and is 0\n");
 	else
-		printf("last dij of %d is %d and is less than 6 and not 0\n", n, last_dij);
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
