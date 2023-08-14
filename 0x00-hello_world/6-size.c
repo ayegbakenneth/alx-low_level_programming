668849994f33be9168ba9110a13724154393e18a
ayegbakenneth <ayegbakenneth>
2023-08-14 05:12:15 -0700

    gcc 6-size.c task commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %lu byte(s)\n", sizeof(char));
-	printf("Size of short: %lu byte(s)\n", sizeof(short));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	printf("Size of double: %lu byte(s) \n", sizeof(double));
-	printf("Size of long double: %lu byte(s)\n", sizeeof(long double));
+	printf("Size of a char is %lu bytes\n", sizeof(char));
+	printf("Size of an int is %lu bytes\n", sizeof(int));
+	printf("Size of a long int is %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int is %lu bytes\n", sizeof(long long));
+	printf("Size of a float is %lu bytes\n", sizeof(float));
 	return (0);
 }
