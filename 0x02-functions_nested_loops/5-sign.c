83dd74345f8b406b5c789a9de27683dd2c406749
ayegbakenneth <ayegbakenneth>
2023-08-15 12:19:51 -0700

    5-sign.c commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - display the number sign
+ * @n: This is the number that will be checked
+ * Return: 1 for positive, -1 for negative or 0 for other numbers
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
