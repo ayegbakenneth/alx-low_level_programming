2d63b768283a942053e0b711894b2ada1707e725
ayegbakenneth <ayegbakenneth>
2023-08-16 12:30:17 -0700

    104-fibonacci.c commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - this is the starting point
+ *description: calculating numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+	unsigned long int x = 0, y = 1, next = 0;
+
+	while (a < 98)
+	{
+		next = x + y;
+		x = y;
+		y = next;
+		printf("%lu", next);
+		if (a < 97)
+			printf(",");
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
