c63f67425a3de29edb6112bcd708964bd44c90c2
ayegbakenneth <ayegbakenneth>
2023-08-18 01:32:45 -0700

    2-mul commit message

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _mul - multiplication of numbers
+ * @a: the first number
+ * @b: - the second number
+ * Return: 0 always
+ */
+int _mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
