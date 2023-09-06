ef5abafb6acacddc63b05c6b35ff184242e4f674
ayegbakenneth <ayegbakenneth@gmail.com>
2023-09-05 23:35:55 -0700

    Write a program that multiplies two number

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * _atoi - Strings into integers
  * @s: string to be converted
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef Main_H
+#define MAIN_H
+
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
